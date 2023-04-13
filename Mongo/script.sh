6d1b2ccb1fc8c22fb15457d00588e4fbd40cea60
rsk38 <60634276+rsk38@users.noreply.github.com>
2023-04-13 14:28:59 -0400

    Update script.sh

--- Mongo/script.sh
+++ Mongo/script.sh
@@ -14,10 +14,10 @@ for i in 1 2 3
 do
 printf "\n##################################################################################\n" >> ../Mongo/testLoad1000.csv
 printf "Loading workoad A try $i\n" >> ../Mongo/testLoad1000.csv
-./bin/ycsb load mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testLoad1000.csv
+./bin/ycsb load mongodb-async -s -P workloads/workload1000 -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testLoad1000.csv
 printf "\n##################################################################################\n" >> ../Mongo/testRun1000.csv
 printf "Running test workoad A try $i\n" >> ../Mongo/testRun1000.csv
-./bin/ycsb run mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testRun1000.csv
+./bin/ycsb run mongodb-async -s -P workloads/workload1000 -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testRun1000.csv
 docker stats --no-stream >> cpu1000.csv
 done
 
@@ -25,10 +25,10 @@ for i in 1 2 3
 do
 printf "\n##################################################################################\n" >> ../Mongo/testLoad0100.csv
 printf "Loading workoad A try $i\n" >> ../Mongo/testLoad0100.csv
-./bin/ycsb load mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testLoad0100.csv
+./bin/ycsb load mongodb-async -s -P workloads/workload0100 -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testLoad0100.csv
 printf "\n##################################################################################\n" >> ../Mongo/testRun0100.csv
 printf "Running test workoad A try $i\n" >> ../Mongo/testRun0100.csv
-./bin/ycsb run mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testRun0100.csv
+./bin/ycsb run mongodb-async -s -P workloads/workload0100 -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testRun0100.csv
 docker stats --no-stream >> cpu0100.csv
 done
 
@@ -36,10 +36,10 @@ for i in 1 2 3
 do
 printf "\n##################################################################################\n" >> ../Mongo/testLoad5050.csv
 printf "Loading workoad A try $i\n" >> ../Mongo/testLoad5050.csv
-./bin/ycsb load mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testLoad5050.csv
+./bin/ycsb load mongodb-async -s -P workloads/workload5050 -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testLoad5050.csv
 printf "\n##################################################################################\n" >> ../Mongo/testRun5050.csv
 printf "Running test workoad A try $i\n" >> ../Mongo/testRun5050.csv
-./bin/ycsb run mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testRun5050.csv
+./bin/ycsb run mongodb-async -s -P workloads/workload5050 -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testRun5050.csv
 docker stats --no-stream >> cpu5050.csv
 done
 
@@ -47,10 +47,10 @@ for i in 1 2 3
 do
 printf "\n##################################################################################\n" >> ../Mongo/testLoad7525.csv
 printf "Loading workoad A try $i\n" >> ../Mongo/testLoad7525.csv
-./bin/ycsb load mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testLoad7525.csv
+./bin/ycsb load mongodb-async -s -P workloads/workload7525 -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testLoad7525.csv
 printf "\n##################################################################################\n" >> ../Mongo/testRun7525.csv
 printf "Running test workoad A try $i\n" >> ../Mongo/testRun7525.csv
-./bin/ycsb run mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/7525.csv
+./bin/ycsb run mongodb-async -s -P workloads/workload7525 -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/7525.csv
 docker stats --no-stream >> cpu7525.csv
 done
 
@@ -58,10 +58,10 @@ for i in 1 2 3
 do
 printf "\n##################################################################################\n" >> ../Mongo/testLoad2575.csv
 printf "Loading workoad A try $i\n" >> ../Mongo/testLoad2575.csv
-./bin/ycsb load mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testLoad12575.csv
+./bin/ycsb load mongodb-async -s -P workloads/workload2575 -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testLoad12575.csv
 printf "\n##################################################################################\n" >> ../Mongo/testRun2575.csv
 printf "Running test workoad A try $i\n" >> ../Mongo/testRun2575.csv
-./bin/ycsb run mongodb-async -s -P workloads/workloada -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testRun2575.csv
+./bin/ycsb run mongodb-async -s -P workloads/workload2575 -p mongodb.url=mongodb://172.31.29.2:27017/ycsb?w=0 >> ../Mongo/testRun2575.csv
 docker stats --no-stream >> cpu2575.csv
 done
 
